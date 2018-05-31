14e49eace93beb892a176ca41256c5ad16fd1896
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-05-30 21:55:18 -0500

    env config

--- bot.js
+++ bot.js
@@ -1,15 +1,22 @@
+// dotenv congig
+require('dotenv').config()
+
 // Twit config
 const Twit = require('twit');
 var T = new Twit({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET       60*1000,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 })
 
-// dotenv congig
-require('dotenv').config()
 
 // Poke-API config
 var Pokedex = require('pokedex-promise-v2');
 var P = new Pokedex();
+
+
+// Post tweet
+T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  console.log(data)
+})
